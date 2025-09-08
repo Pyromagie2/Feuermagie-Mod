6ba430b48dcb3a9cd9f5ca0cb4b3605110372835
Pyromagie2 <160120693+Pyromagie2@users.noreply.github.com>
2025-09-08 21:25:59 +0200

    Erstes Item geaddet

--- Minecraft-Coding/feuer-magie-template-1.21.8/src/main/java/net/feuermagie/pyromagie/FeuerMagie.java
+++ Minecraft-Coding/feuer-magie-template-1.21.8/src/main/java/net/feuermagie/pyromagie/FeuerMagie.java
@@ -2,23 +2,16 @@ package net.feuermagie.pyromagie;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.feuermagie.pyromagie.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FeuerMagie implements ModInitializer {
 	public static final String MOD_ID = "feuer-magie";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+        ModItems.registerModItems();
 	}
 }
