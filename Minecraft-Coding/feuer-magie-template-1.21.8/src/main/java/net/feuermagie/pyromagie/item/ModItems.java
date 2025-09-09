486540e87708d6d0440e45e601f7d16ebdeabd6b
Pyromagie2 <160120693+Pyromagie2@users.noreply.github.com>
2025-09-09 10:25:50 +0200

    pink garnet entfernt addet fire ingot

--- Minecraft-Coding/feuer-magie-template-1.21.8/src/main/java/net/feuermagie/pyromagie/item/ModItems.java
+++ Minecraft-Coding/feuer-magie-template-1.21.8/src/main/java/net/feuermagie/pyromagie/item/ModItems.java
@@ -12,8 +12,10 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    public static final Item PINK_GARNET = registerItem("pink_garnet",new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM,Identifier.of(FeuerMagie.MOD_ID+":pink_garnet")))));
+
     public static final Item FIRE_ESSENCE = registerItem("fire_essence",new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM,Identifier.of(FeuerMagie.MOD_ID+":fire_essence")))));
+    public static final Item FIRE_INGOT = registerItem("fire_ingot",new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM,Identifier.of(FeuerMagie.MOD_ID+":fire_ingot")))));
+
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(FeuerMagie.MOD_ID, name), item);
     }
@@ -22,8 +24,8 @@ public class ModItems {
         FeuerMagie.LOGGER.info("Registering ModItems" + FeuerMagie.MOD_ID);
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-            entries.add(PINK_GARNET);
             entries.add(FIRE_ESSENCE);
+            entries.add(FIRE_INGOT);
         });
     }
 
